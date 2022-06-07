5e751f7848c826b354141b0e2fce5f1fa0e0935b
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-06-06 23:06:07 -0500

    Fix CORS

--- src/http-common.js
+++ src/http-common.js
@@ -1,4 +1,13 @@
 import axios from "axios";
+// export default axios.get('http://localhost:3000/explorers')
+// .then(function (response){
+//   console.log("Response: " + response);
+// }).catch(function(error){
+//   console.log("This is an error: " + error);
+// }).then(function(){
+//   console.log("end");
+// })
+
 export default axios.create({
   baseURL: "http://localhost:3000",
   headers: {
